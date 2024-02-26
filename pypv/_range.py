3c6b09b58c379597a86565c3a48f3a5371ae185f
Patrick Pasquini <contatopasquini@gmail.com>
2024-02-26 17:21:29 -0300

    🚧 Comment solar_physics, need refactor

--- pypv/_range.py
+++ pypv/_range.py
@@ -2,6 +2,17 @@ from typing import Optional
 
 
 class Range:
+    """
+    This class is used to define a range of values, such as temperature or voltage, with optional
+    minimum and maximum bounds and a fixed ratio.
+
+    Args:
+        min (Optional[float]): The optional minimum value of the range.
+        max (Optional[float]): The optional maximum value of the range.
+        ratio (int): The fixed ratio associated with the range.
+
+    """
+
     min: Optional[float] = None
     max: Optional[float] = None
     ratio: int
